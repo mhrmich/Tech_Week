2e1d24576287e39e8558a0b3835e67cc4d070578
mhrmich <mhrmich@outlook.com>
2025-10-17 06:30:36 -0700

    message

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  outputFileTracingRoot: __dirname,
+}
+
+export default nextConfig
